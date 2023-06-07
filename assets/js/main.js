99d5b7a6b1b265744660132dc8afb7e2ea049599
Felipe Lima <102830741+LipePLima@users.noreply.github.com>
2023-06-07 10:13:09 -0300

    feat: confirmação de adição ao carrinho por meio de 'alert' ao clicar no botão 'adicionar ao carrinho'

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,10 @@
 const moreLess = document.querySelectorAll('.quantify__btn');
+const addBtn   = document.querySelector('.btn-buy');
+const span  = document.querySelector('.quantify__num');
+let numSpan = parseInt(span.textContent);
 
 moreLess.forEach( btn => {
      btn.addEventListener('click', () => {
-          const span  = document.querySelector('.quantify__num');
-          let numSpan = parseInt(span.textContent);
-
           if(btn.classList.contains('quantify__add')) {
                numSpan += 1
                span.textContent = numSpan
@@ -15,4 +15,8 @@ moreLess.forEach( btn => {
                }
           }
      })
+});
+
+addBtn.addEventListener('click', () => {
+     alert(`${span.textContent} Gabrielle Essence Eau De Parfum adicionado(s) ao carrinho`)
 })
